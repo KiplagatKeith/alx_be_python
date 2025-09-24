613191159cb5ad31c7ef75271e95bede1f8c7b7c
Kiplagat Keith <kiplagatkeith@proton.me>
2025-09-24 12:10:16 +0300

    Data structures quiz

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,17 +3,18 @@ from datetime import datetime, timedelta, date
 # Part 1: Display current date and time
 def display_current_datetime():
     current_date = datetime.now()
-    # Format as "YYYY-MM-DD HH:MM:SS"
+    # Must return formatted string
     return current_date.strftime("%Y-%m-%d %H:%M:%S")
 
 print("Current date and time:", display_current_datetime())
 
 # Part 2: Calculate a future date
-number_of_days = int(input("Enter number of days: "))
-
-def calculate_future_date():
+def calculate_future_date(number_of_days):
     future_date = date.today() + timedelta(days=number_of_days)
-    # Format as "YYYY-MM-DD"
+    # Must return formatted string
     return future_date.strftime("%Y-%m-%d")
 
-print("Future date:", calculate_future_date())
+# Ask user for days
+number_of_days = int(input("Enter number of days: "))
+
+print("Future date:", calculate_future_date(number_of_days))
