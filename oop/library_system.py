5356452bc407c103cd79abb4e0e8eed9c9459075
Kiplagat Keith <kiplagatkeith@proton.me>
2025-10-10 02:47:55 +0300

    oop

---
+++ oop/library_system.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+    
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_books(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book, EBook, or PrintBook instances can be added.")  
+
+    def list_books(self):
+        if not self.books:
+            print("No books in the library.")
+            return
+        for book in self.books:
+            print(book)
